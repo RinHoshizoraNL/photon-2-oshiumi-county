e2b2c0c1b153fc4f734ce76168fb4021db48611c
Rin Hoshizora <hoshizora-designs@outlook.com>
2026-02-24 15:46:26 +0100

    Siren tweaks

--- lua/photon-v2/library/vehicles/photon2_rin_chevy_caprice91_oshi_sheriff.lua
+++ lua/photon-v2/library/vehicles/photon2_rin_chevy_caprice91_oshi_sheriff.lua
@@ -125,6 +125,36 @@ VEHICLE.Equipment = {
 		}
 	},
 	{
+		Category = "Siren",
+		Options = {
+			{
+				Option = "Federal Signal Smart Siren",
+				Components = {
+					{
+						Name = "@siren_speaker",
+						Component = "siren_prototype",
+						Model = "models/gandhi/props/es100.mdl",
+						Position = Vector( 0, 104, 23.5 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 1,
+						Siren = "pa300_2",
+						Templates = {
+							["Sound"] = { 
+								Tone = {
+									DSP = 0,
+									Pitch = 100
+								}
+							}
+						},
+						Inputs = { 
+							["Emergency.SirenParkKill"] = { ["PARK"] = {} }
+						}
+					},
+				}
+			}
+		}
+	},
+	{
 		Category = "Spotlight",
 		Options = {
 			{
